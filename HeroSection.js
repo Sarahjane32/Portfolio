db5afdbafafde3addd3744f123863f59650f16e4
Sarah Jane <sarahjane.taroy@cvsu.edu.ph>
2026-05-07 16:04:03 +0800

    First commit, nagawa ko na ang navigation pero hindi pa final

---
+++ HeroSection.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleBtn = document.getElementById("toggleTheme");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    toggleBtn.textContent =
+      document.body.classList.contains("dark") ? "☀️" : "🌙";
+  });
+});
