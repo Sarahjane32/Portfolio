1cad72aeb37506038d045c4c966a327cd2b9af56
Sarah Jane <sarahjane.taroy@cvsu.edu.ph>
2026-05-07 17:10:21 +0800

    Second Commit, Sa Hero Section ko nalagyan ko na ng mga text at picture

--- HeroSection.js
+++ HeroSection.js
@@ -1,3 +1,30 @@
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.addEventListener("scroll", () => {
+  let top = window.scrollY;
+
+  sections.forEach(sec => {
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+
+      // alisin lahat ng active
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+      });
+
+      // lagay sa current section lang
+      let activeLink = document.querySelector(`header nav a[href="#${id}"]`);
+      if (activeLink) {
+        activeLink.classList.add("active");
+      }
+    }
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const toggleBtn = document.getElementById("toggleTheme");
 
